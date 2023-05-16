b86bf354ca929d3c58db847fd96017174387701d
abykm <abykm164@gmail.com>
2023-05-16 14:37:16 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Login from './Login';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen1234 :)</p>
+      <p>Start editing to see some magic happen12343 :)</p>
+      <Login />
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,30 @@
+import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
+
+export default function Login() {
+  return (
+    <div className="App">
+      <h2>Sign In</h2>
+      <Form className="form">
+        <FormGroup>
+          <Label for="exampleEmail">Username</Label>
+          <Input
+            type="email"
+            name="email"
+            id="exampleEmail"
+            placeholder="example@example.com"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            id="examplePassword"
+            placeholder="********"
+          />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    </div>
+  );
+}
